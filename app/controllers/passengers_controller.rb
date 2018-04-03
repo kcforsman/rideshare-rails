0d10a3e0602d6839cde95225a6ccdeeb68a99d5f
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-02 17:39:05 -0700

    added controller for passengers and associated routes.

---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,23 @@
+class PassengersController < ApplicationController
+  def index
+    @passenger = Passenger.all
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  get '/passengers', to: "passengers#index", as: "passengers"
+  get '/passengers/new', to: "passengers#new", as: "new_passenger"
+  post '/passengers', to: "passengers#create"
+  get '/passengers/:id/edit',  to: "passengers#edit", as: "edit_passenger"
+
+  get '/passengers/:id', to: "passenger#show", as:"passenger"
+
+  patch '/passengers/:id', to: "passenger#update"
+  delete '/passengers/:id', to: "passenger#destroy"
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
