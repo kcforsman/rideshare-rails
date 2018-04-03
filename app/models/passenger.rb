74ae9ebb67d926520d2f100ec80d0ac2f9e2f8e4
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-03 00:18:24 -0700

    added logic to calculate total charged to passenger and display it in the passenger show view

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,9 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def calculate_total_charged
+    total_amount = 0
+    self.trips.each { |trip| total_amount += trip.cost }
+    ((total_amount.to_f)/100).round(2)
+  end
 end
