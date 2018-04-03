7020ccd1eee9954ee2eea50685f763fc03140723
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-03 11:09:19 -0700

    updated routes to shorthand

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,19 @@
 Rails.application.routes.draw do
 
 #routes for trips
-get '/trips', to: "trips#index", as: "trips"
-get '/trips/new', to: "trips#new", as: "new_trip"
-post '/trips', to: "trips#create"
-get '/trips/:id/edit',  to: "trips#edit", as: "edit_trip"
+  delete '/trips/:id', to: "trips#destroy"
 
-get '/trips/:id', to: "trips#show", as:"trip"
-
-patch '/trips/:id', to: "trips#update"
-delete '/trips/:id', to: "trips#destroy"
-
+  resources :trips, except: [:index]
 
 # routes for drivers
-  get '/drivers', to: "drivers#index", as: "drivers"
-  get '/drivers/new', to: "drivers#new", as: "new_driver"
-  post '/drivers', to: "drivers#create"
-  get '/drivers/:id/edit',  to: "drivers#edit", as: "edit_driver"
-
-  get '/drivers/:id', to: "drivers#show", as:"driver"
-
-  patch '/drivers/:id', to: "drivers#update"
   delete '/drivers/:id', to: "drivers#destroy"
 
+  resources :drivers
+
 # routes for passengers
-  get '/passengers', to: "passengers#index", as: "passengers"
-  get '/passengers/new', to: "passengers#new", as: "new_passenger"
-  post '/passengers', to: "passengers#create"
-  get '/passengers/:id/edit',  to: "passengers#edit", as: "edit_passenger"
-
-  get '/passengers/:id', to: "passengers#show", as:"passenger"
-
-  patch '/passengers/:id', to: "passengers#update"
   delete '/passengers/:id', to: "passengers#destroy"
 
-
-
+  resources :passengers
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
