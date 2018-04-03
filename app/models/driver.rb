ee08d52ece233aa46d7b86a320e2761a94ab3eb2
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-03 14:14:52 -0700

    Addlinks to edit and delete a driver. Also added the ability to see all trips associated with a specific driver and their passenger. Finally, added the method within the driver controller to calculate the amount earned.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,10 @@
 class Driver < ApplicationRecord
   has_many :trips
+  def calculate_total_earned
+    total_earned = 0
+    self.trips.each do |trip|
+      total_earned+=((trip.cost-165)*(0.8))
+    end
+    return (total_earned/100).round(2)
+  end
 end
