1fa07b0b2e2198d31af22ec61cb07546bd1e08b7
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-03 17:29:57 -0700

    added links to a trips driver and passenger from the trips detail page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,18 +4,33 @@ class TripsController < ApplicationController
     @trip = Trip.find_by(id: params[:id])
   end
 
-  def create
+  def new
+    @trip = Trip.new
   end
 
-  def new
+  def create
+    # @trip = Trip.new(trip_params)
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id])
   end
 
   def update
   end
 
   def destroy
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.destroy
+      redirect_to drivers_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:rating)
   end
 end
